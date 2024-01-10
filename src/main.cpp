d2678eb9e3b6ab8301186aa147b8803e1f739b12
jake-is-ESD-protected <jt@tschavoll.at>
2024-01-10 22:31:08 +0100

    add dummy program

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,11 @@
 #include <Arduino.h>
 
-class SomeClass{
-  public:
-    String Foo;
-    int SomeInteger;
-};
-
-enum SomeEnum {
-  EnumValueA,
-  EnumValueB,
-  EnumValueC
-};
-
-struct SomeStruct{
-  double FieldA;
-  bool FieldB;
-  SomeEnum FieldC;
-};
-
-unsigned long nextTime = 5000;
-SomeClass someClass;
-SomeStruct someStruct;
+uint8_t counter;
 
 void setup() {
-  Serial.begin(115200);
-  // Initialise someClass
-  someClass.Foo = "Bar";
-  someClass.SomeInteger = 1337;
-  // Initialise someStruct
-  someStruct.FieldA = 123.456;
-  someStruct.FieldB = true;
-  someStruct.FieldC = EnumValueA;
-  Serial.println("Initialised"); // Set a Breakpoint on this line to interrogate someClass and someStruct
-}
+    counter = 0;    // if you break here, counter may be 0 or any other uint8_t value
+}   // if you break here, counter will definitely be 0
 
 void loop() {
-  if (millis() < nextTime) { return; } // Code won't proceed further until we reach the nextTime. A Breakpoint here will hit every time loop() is called
-  // Breakpoints below this line will only hit every time the device counts 5 seconds!
-  someClass.SomeInteger = someClass.SomeInteger * 2; // We double the value every 5 seconds
-  someStruct.FieldA = someStruct.FieldA * 3; // We triple the value every 5 seconds
-  nextTime = millis() + 5000; // nextTime = Now + 5 Seconds. Place a Breakpoint here to look at the updated values above!
+    counter++;  // every time you break here, counter will increment by one and jump from 255 to 0
 }
